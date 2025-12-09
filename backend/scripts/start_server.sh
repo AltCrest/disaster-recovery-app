62f4bea64eaf685ecce59fcde153fcae89176268
AltCrest <149634517+AltCrest@users.noreply.github.com>
2025-12-09 17:19:51 -0500

    update name

--- backend/scripts/start_server.sh
+++ backend/scripts/start_server.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 # This script starts the application server.
+
+# Change to the application directory
 cd /opt/app/backend
+
+# Load the environment variables from the .env file
+if [ -f .env ]; then
+  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+fi
+
+# Start the Gunicorn server in the background
 gunicorn --bind 0.0.0.0:5000 --daemon app:app
