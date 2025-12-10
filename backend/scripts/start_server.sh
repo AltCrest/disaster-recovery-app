e01694b4c3827bc60fccbc94f2a9b2aeb88112e9
AltCrest <149634517+AltCrest@users.noreply.github.com>
2025-12-10 01:29:22 -0500

    retrig

--- backend/scripts/start_server.sh
+++ backend/scripts/start_server.sh
@@ -5,7 +5,6 @@ cd /opt/app/backend
 
 if [ -f .env ]; then
   echo "Loading environment variables from .env file..."
-  # This command exports the variables so Gunicorn can see them
   export $(cat .env | sed 's/#.*//g' | xargs)
 else
   echo "Warning: .env file not found. Application may not be configured correctly."
