c2b01cc471f360373f461cbdff30375ef1c6923c
AltCrest <149634517+AltCrest@users.noreply.github.com>
2025-12-09 23:30:09 -0500

    retrig

--- backend/scripts/install_dependencies.sh
+++ backend/scripts/install_dependencies.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# This script installs the application dependencies.
 yum install -y python3-pip
 pip3 install gunicorn
 pip3 install -r /opt/app/backend/requirements.txt
--- frontend/scripts/clean_html.sh
+++ frontend/scripts/clean_html.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# This script runs before the new frontend files are copied.
-# It cleans out the old website to ensure a clean deployment.
 rm -rf /usr/share/nginx/html/*
